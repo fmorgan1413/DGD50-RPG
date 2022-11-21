a5a4f641c2933aa22e004fcd90f4cc28904168db
fmorgan1413 <91088434+fmorgan1413@users.noreply.github.com>
2022-11-21 13:05:59 -0500

    player/game start entrances (screen fade gucci, players move on screen gucci) (HAILEY PLZ LEAVE ME ALONE I WANT TO BE WITH MY FAMILY ON THE HOLIDAYS AAAAHHHHHHHHH)

--- DGD50 RPG/Assets/Scripts/ScreenFade.cs
+++ DGD50 RPG/Assets/Scripts/ScreenFade.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,10 +11,15 @@ public class ScreenFade : MonoBehaviour
 
     public GameObject player1;
     public GameObject player2;
+
+    private Vector3 target1 = new Vector3(2.63f, 2.31f, 0.0f);
+    private Vector3 target2 = new Vector3(3.35f, -1.18f, 0.0f);
+
+    private float time = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(FadeSquare());
+        StartCoroutine(StartScreen(player1, player2, target1, target2, time));
     }
 
     // Update is called once per frame
@@ -22,7 +28,7 @@ public class ScreenFade : MonoBehaviour
         
     }
 
-    public IEnumerator FadeSquare(bool fadeToNotBlack = true, int fadeSpeed = 20)
+    public IEnumerator FadeSquare(bool fadeToNotBlack = true, int fadeSpeed = 15)
     {
         Color objectColor = fadingSquare.GetComponent<Image>().color;
         float fadeAmount;
@@ -39,5 +45,26 @@ public class ScreenFade : MonoBehaviour
             }
         }
 
+    }
+
+    public IEnumerator StartScreen(GameObject player1, GameObject player2, Vector3 targetPos1, Vector3 targetPos2, float duration) 
+    {
+        StartCoroutine(FadeSquare());
+        yield return new WaitForSeconds(.5f);
+
+        float time = 0;
+        Vector3 startPos1 = player1.transform.position;
+        Vector3 startPos2 = player2.transform.position;
+
+        while (time < duration)
+        {
+            player1.transform.position = Vector3.Lerp(startPos1, targetPos1, time / duration);
+            player2.transform.position = Vector3.Lerp(startPos2, targetPos2, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        player1.transform.position = targetPos1;
+        player2.transform.position = targetPos2;
     }
 }
